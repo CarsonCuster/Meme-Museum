24573f6f5bdb5dc63a331d02a11ad78f317d4595
Carson.Custer <carsonwcuster@gmail.com>
2020-04-20 12:09:49 -0400

    Added PlayVideo script

---
+++ Assets/Scripts/PlayVideo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayVideo : MonoBehaviour
+{
+    public GameObject screen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var videoPlayer = screen.GetComponent<UnityEngine.Video.VideoPlayer>();
+            videoPlayer.Pause();
+    }
+
+    // Update is called once per frame
+    void OnCollisionEnter()
+    {
+        var videoPlayer = screen.GetComponent<UnityEngine.Video.VideoPlayer>();
+        if(videoPlayer.isPaused)
+        {
+            videoPlayer.Play();
+        }
+        else
+        {
+            videoPlayer.Pause();
+        }
+    }
+}
